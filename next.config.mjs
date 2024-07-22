c354cf0dbafd79219d55e0a836614f85564dd63f
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-07-22 15:22:51 +0700

    update ......

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: 'http://localhost:9999/api',
+    // API_URL: 'http://localhost:9999/api',
+    // Prod
+    API_URL: 'https://be.trinhminhnhat.info.vn/api',
   },
-
-  // dev environment
-  // env: {
-  //   API_URL: 'https://api.vk169.net/api',
-
-  // },
-
-  // Production environment
-  // env: {
-  //   API_URL: 'https://api.ku3933d.net/api',
-
-  // },
   images: {
     domains: ['storage.googleapis.com'],
   },
