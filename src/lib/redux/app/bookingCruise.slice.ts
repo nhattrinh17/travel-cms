f4d6ab2a5327b322c19bfd15764a12d1a63f7d0a
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-08-13 17:26:32 +0700

    update

--- src/lib/redux/app/bookingCruise.slice.ts
+++ src/lib/redux/app/bookingCruise.slice.ts
@@ -43,6 +43,7 @@ const bookingCruiseSlice = createSlice({
     setLimitOrPageBookingCruise: (state, action: { payload: { limit?: number; page?: number } }) => {
       state.limit = action.payload.limit ? action.payload.limit : state.limit;
       state.page = action.payload.page ? action.payload.page : state.page;
+      state.refreshData = true;
     },
     resetDataBookingCruise: (state) => {
       state.bookingCruises = [];
