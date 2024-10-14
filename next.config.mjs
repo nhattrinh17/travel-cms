fca79d020965da8d742764b9b143d23fe8bb91dc
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-10-14 16:20:00 +0700

    add docker file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: 'http://localhost:9999/api',
+    // API_URL: 'http://localhost:9999/api',
     // Prod
-    // API_URL: 'https://be.trinhminhnhat.info.vn/api',
+    API_URL: 'https://be.thglobaltravel.com/api',
   },
   images: {
     domains: ['storage.googleapis.com'],
     unoptimized: true,
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
